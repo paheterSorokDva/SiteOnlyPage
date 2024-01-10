b117ef9537812934a28d8046ec70f770f40365ef
Paheter <111425098+paheterSorokDva@users.noreply.github.com>
2024-01-10 21:29:34 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 
-const mobileBtn = document.getElementById('mobile-cta')
-const nav = document.querySelector('nav')
-const mobileBtnExit = document.getElementById('mobile-exit');
+const mobileBtn = document.getElementById('mobile-cta') // btn menu
+            nav = document.querySelector('nav')
+            mobileBtnExit = document.getElementById('mobile-exit');
 
-mobileBtn.addEventListener('click', () =>{
-    nav.add
-    nav.ClassList.add('menu-btn');
-});
+         mobileBtn.addEventListener("click", () => {
+            
+            nav.classList.add("menu-btn");
+         });
+
+         mobileBtnExit.addEventListener("click", () => {
+            
+            nav.classList.remove("menu-btn");
+         });
